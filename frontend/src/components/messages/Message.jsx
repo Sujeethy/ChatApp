465659bd6c87f6574ca067b8aaab5bb92e10c146
Sujeethy <73529876+Sujeethy@users.noreply.github.com>
2024-07-30 01:20:08 +0530

    Update Message.jsx

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,7 +1,6 @@
 import { useAuthContext } from "../../context/AuthContext";
 import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
-import { Button } from "react-bootstrap";
 import React, { useState } from "react";
 
 const Message = ({ message }) => {
